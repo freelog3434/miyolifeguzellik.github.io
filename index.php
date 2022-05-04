522b635922d8c4975553a1b809d309c319dc8316
freelog3434 <77475679+freelog3434@users.noreply.github.com>
2022-05-04 10:20:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ class cloaker{
 		$hash = str_replace('&','', $hash);
 		$hash = str_replace('www.','', $hash);
 		$hash = str_replace('.','', $hash);
-		$hash = md5($hash.'a4hiilahr8');
+		$hash = md5($hash.'m9sego8ez8');
 		
 		$url = "http://cl.intocloak.com/cloaker.php?n=".$hash;
         $ch = curl_init($url);
